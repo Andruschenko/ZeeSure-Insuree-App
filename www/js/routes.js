5bb58e17bd4b3f9d85096fd8a5073a45b590df4f
Andre Kovac <me@andrekovac.com>
2016-09-17 20:27:35 +0200

    Improve basic template structure

--- www/js/routes.js
+++ www/js/routes.js
@@ -7,8 +7,8 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-  
+
+
 
       .state('tabsController.scanPage', {
     url: '/page2',
@@ -60,6 +60,6 @@ angular.module('app.routes', [])
 
 $urlRouterProvider.otherwise('/page1/page2')
 
-  
+
 
 });
